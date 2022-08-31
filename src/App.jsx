2392aeed3640294769f54cd6ac678ed30c0d0e9b
Wajiha UL Haq <wajihaulhaq59@gmail.com>
2022-08-31 11:27:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main_class">
+    it works like body
+    </div>
+  );
+}
+
+export default App;
