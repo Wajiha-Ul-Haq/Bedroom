934a1624f66cea1c3ba237af7df858bef56509f8
Wajiha UL Haq <wajihaulhaq59@gmail.com>
2022-09-02 14:11:58 +0100

    css error

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="main_class">
-    it works like body
-    You you can start your app 
+    <div className="navbar">
+        
+        <h3>INTERIOR</h3>
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">BEDROOM</a></li>
+          <li><a href="#">DINING</a></li>
+          <li><a href="#">KITCHEN</a></li>
+          <li><a href="#">BACKYARD</a></li>
+        </ul>
+       </div>
+        <div className="main">
+            <h1>DESIGN  YOUR HOUSE</h1>
+            <p>Subscribe Easy Tutorial Youtube channel to watch more videos,<br/>press the bell icon to get latest update</p>
+             <button> <span/>WATCH MORE</button>
+             <button> <span/>SUBSCRIBE</button>
+        </div>
     </div>
   );
 }
